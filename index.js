001ac3ad23e533385e20a939b0fa97952ca2cb8c
Cory Pollard <astandard87@gmail.com>
2022-12-05 10:58:08 -0600

    Added notes about server side routing

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 const { text } = require('stream/consumers');
+const http = require('http');
+const url = require('url');
+
+/////////////////////////////////////////////////////////////////
+// FILES
+////////////////////////////////////////////////////////////////
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -10,17 +16,41 @@ const { text } = require('stream/consumers');
 // console.log('File written.');
 
 // Non-blocking, asynchronous way
-fs.readFile('./txt/starttttt.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('Error!');
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
+// fs.readFile('./txt/starttttt.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('Error!');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Your file has been written');
-      });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file!');
+
+///////////////////////////////////////////////////////////////////////////
+// SERVER
+///////////////////////////////////////////////////////////////////////////
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
     });
-  });
+    res.end('<h1>Page not found</h1>');
+  }
 });
-console.log('Will read file!');
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
