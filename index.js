c3cc7e1013954f533acf70bc0c2bd17b3069caed
Cory Pollard <astandard87@gmail.com>
2022-12-05 13:25:11 -0600

    Added programable values to the product template and the logic to show on page

--- index.js
+++ index.js
@@ -34,19 +34,67 @@ const url = require('url');
 ///////////////////////////////////////////////////////////////////////////
 // SERVER
 ///////////////////////////////////////////////////////////////////////////
+
+// We use a regular expression /{item}/g    -and use the g(global) modifier to access each appearance of these
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+
+  return output;
+};
+
+// readFileSync = synchronous, blocking version
+// readFile = asynchronous, non-blocking version
+// These templates don't change, so we can have them at the top level in a synchronous way.
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // Overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW');
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
+
+    // Product page
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+
+    // API
   } else if (pathName === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
